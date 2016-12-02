fe79994628d95504b162281529f984bc98032b80
Jaden Palmer-Leandre <jadenpalmerleandre@hotmail.co.uk>
2016-12-02 13:28:40 +0000

    :(

---
+++ sample.cc
@@ -0,0 +1,144 @@
+#include <iostream>
+
+#include "sample.h"
+
+
+using namespace std;
+
+int main_test(int argc, char *argv[]) {
+	/* an empty sample object - initialise with a vector once
+	 * you have defined the sample constructor that takes a
+	 * vector
+	 */
+
+  //TODO possibly change all for loops to iterators - more efficient
+
+  // definition of member variable(s)
+  vector<long double> numbers = {7, 11 ,2 ,13 ,3 ,5};
+
+
+  //creating a new sample object called "a_sample"
+  sample a_sample(numbers); // = { 7, 11, 2, 13, 3, 5};
+   
+	cout << "\tBefore city_test()\n";
+	city_test(a_sample);
+	cout << "\tAfter city_test()\n";
+
+	/* Place your code for testing sample after this line. */
+
+
+//	a_sample.print();
+
+	// print vector contents from "a_sample"
+	cout << a_sample << endl;
+
+
+	// print test for mathematical functions
+	cout << a_sample.minimum() << endl;
+	cout << a_sample.maximum() << endl;
+	cout << a_sample.get_data() << endl;
+	cout << a_sample.range() << endl;
+	cout << a_sample.midrange() << endl;
+	cout << a_sample.mean() << endl;
+	cout << a_sample.variance() << endl;
+	cout << a_sample.std_deviation() << endl;
+	cout << a_sample.median() << endl;
+
+
+	//	a_sample.get_data().print();
+
+	return 0;
+}
+
+void sample::print() {
+  // prints sample vector to an output stream
+  cout << "<" << numbers.size() << ": ";
+   for(vector<long double>::iterator i = numbers.begin(); i!= numbers.end(); i++){
+       cout << *i << " ";
+  }
+   cout << ">";
+}
+
+long double sample::minimum() {
+  long double n = numbers.size();
+  long double min = numbers[0];
+
+  for (int i = 0; i < n; i++){
+    if (numbers[i] < min){
+      min = numbers[i];
+    }
+  }
+  return min;
+}
+
+long double sample::maximum() {
+  long double n = numbers.size();
+  long double max = numbers[0];
+
+  for (int i = 0; i < n; i++){
+    if (numbers[i] > max){
+      max = numbers[i];
+    }
+  }
+  return max;
+}
+
+long double sample::range() {
+   long double max = maximum();
+   long double min = minimum();
+  long double range = max - min;
+
+  return range;
+}
+
+long double sample::midrange() {
+  long double max = maximum();
+  long double min = minimum();
+  long double midrange = (max + min)/2;
+
+  return midrange;
+}
+
+long double sample::mean(){
+  long double n = numbers.size();
+  long double total = 0;
+  long double mean = 0;
+  for (int i = 0; i < n; ++i){
+    total = total + numbers[i]; 
+  }
+  mean = total/n;
+  return mean;
+}
+
+long double sample::variance(){
+  long double m = mean();
+  long double n = numbers.size();
+  long double tmp = 0;
+
+  for (int i = 0; i < n; i++){
+    tmp += (numbers[i] - m)*(numbers[i] - m);
+  }
+  return tmp/n;
+}
+
+long double sample::std_deviation(){
+  long double var = variance();
+  long double std = sqrt(var);
+
+  return std;
+}
+
+long double sample::median(){
+  size_t n = numbers.size();
+  long double median = 0;
+
+  sort(numbers.begin(), numbers.end());
+
+  if (n % 2 == 0){
+    median = (numbers[n/2 - 1] + numbers[n/2])/2;
+	      }
+  else {
+    median = numbers[n/2];
+    }
+    return median;
+}
